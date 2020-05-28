8ce33c77d681ade79eb2ac1e05090ce1145bd700
Smoothini <smoothini@gmail.com>
2020-05-28 03:50:49 -0700

    Genesis
    Starting the journey ...

---
+++ HTML/myScript.js
@@ -0,0 +1,15 @@
+
+
+let Smoothini = function() {
+    let _name = window.prompt("Your Name?");
+    document.getElementById("myTry").innerHTML = "Hello " + _name + "! This is Smoothini's test example."
+};
+
+
+// On line 129 of My Blog
+// document.getElementById('demo').innerHTML = "Testing this out";
+
+
+//document.write("I guess I can write whatever the fuck I want down here.");
+
+console.log("What are all these errors?");
